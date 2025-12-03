127ee693c5c423c52efe74ae1522671b0e10de91
insomnes <denginm@gmail.com>
2025-12-03 02:50:55 +0100

    solution: 2025 day 02 part 02

--- src/pyaoc/y_2025/d_02.py
+++ src/pyaoc/y_2025/d_02.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from functools import cached_property
+from functools import cached_property, lru_cache
 
 from pyaoc.solution import Solution
 
@@ -19,6 +19,10 @@ class IDRange:
         start = int(start_str)
         end = int(end_str)
         return cls(start=start, s_len=len(start_str), end=end, e_len=len(end_str))
+
+    @cached_property
+    def has_eq_lengths(self) -> bool:
+        return self.s_len == self.e_len
 
     @cached_property
     def is_simple_even(self) -> bool:
@@ -62,6 +66,10 @@ type ParsedInput = list[IDRange]
 
 
 def find_invalid_p1(idr: IDRange) -> list[int]:
+    """
+    Invalid ids are made only of some sequence of digits repeated twice.
+    So, 55 (5 twice), 6464 (64 twice), and 123123 (123 twice) would all be invalid IDs.
+    """
     n_len = idr.s_len
     div = 10 ** (n_len // 2)
 
@@ -75,6 +83,76 @@ def find_invalid_p1(idr: IDRange) -> list[int]:
             nums.append(mirror)
 
     return nums
+
+
+def find_invalid_p2(idr: IDRange) -> set[int]:  # noqa: C901
+    """
+    Now invalid ids are made only of some sequence of digits repeated at least twice.
+    So, 12341234 (1234 two times), 123123123 (123 three times), 1212121212 (12 five times),
+    and 1111111 (1 seven times) are all invalid IDs.
+    """
+    if idr.has_eq_lengths:
+        min_repeats = next(i for i in range(2, idr.s_len + 1) if idr.s_len % i == 0)
+    else:
+        s_min = next((i for i in range(2, idr.s_len + 1) if idr.s_len % i == 0), idr.s_len + 1)
+        e_min = next((i for i in range(2, idr.e_len + 1) if idr.e_len % i == 0), idr.e_len + 1)
+        min_repeats = min(s_min, e_min)
+
+
+    nums = set()
+
+    @lru_cache
+    def check_repeats(r: int, num: int) -> bool:
+        nonlocal nums
+        rep_num, pow_mul = num, 10 ** (len(str(num)))
+
+        for _ in range(r - 1):
+            rep_num = rep_num * pow_mul + num
+        if rep_num > idr.end:
+            return False
+        if idr.start <= rep_num <= idr.end:
+            nums.add(rep_num)
+        return True
+
+    max_repeats = idr.s_len if idr.has_eq_lengths else idr.e_len
+
+    @lru_cache
+    def find_invalids_in_range(start: int, end: int, reps: int) -> None:
+        s_len = len(str(start))
+        n_start, n_end = start, end
+        p_mul = 10 ** (s_len // reps)
+
+        for _ in range(reps - 1):
+            n_start = n_start // p_mul
+            n_end = n_end // p_mul
+
+        for n in range(n_start, n_end + 1):
+            if not check_repeats(reps, n):
+                break
+
+
+    for r in range(min_repeats, max_repeats + 1):
+        if idr.s_len % r != 0 and idr.e_len % r != 0:
+            continue
+
+        if idr.has_eq_lengths:
+            find_invalids_in_range(idr.start, idr.end, r)
+            continue
+
+        if idr.s_len % r == 0:
+            new_start = idr.start
+            new_end = (10 ** idr.s_len) - 1
+        elif idr.e_len % r == 0:
+            new_start = 10 ** idr.s_len
+            new_end = idr.end
+        else:
+            raise RuntimeError("Should not be here")
+
+        find_invalids_in_range(new_start, new_end, r)
+
+    return nums
+
+
 
 
 
@@ -104,4 +182,16 @@ class Solution250201(Solution[ParsedInput]):
         return sum(all_nums)
 
 
+class Solution250202(Solution250201):
+    PART: int = 2
+
+    def solve(self) -> int:
+        all_nums = set()
+        for idr in self.parsed_input:
+            invalids = find_invalid_p2(idr)
+            all_nums.update(invalids)
+        return sum(all_nums)
+
+
 Solution250201.register()
+Solution250202.register()
